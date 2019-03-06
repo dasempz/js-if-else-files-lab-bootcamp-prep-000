12a54cbd5e1728433a050814b292927375ca440c
Andrea H <andhaveman@hotmail.com>
2019-03-06 16:24:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 var name = "Susan"
 var height = 70;
+=======
+var name = "Joe"
+var height = "74"
+>>>>>>> 25693977bb840bff9ebccfa4799cf4e177be5608
  
 // Don't worry about this
 module.exports = { name, height
